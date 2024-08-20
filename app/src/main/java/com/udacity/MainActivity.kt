7d0f71997d729f24afd6717e24b5f5fdeb827b91
Jose Villalobos Martinez <villalobosmartinezjosedavid@gmail.com>
2024-08-19 22:00:22 -0600

    Showing notification

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.udacity
 
+import android.Manifest
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        createChannel(channelId = CHANNEL_ID, channelName = getString(R.string.notification_channel))
 
         binding.content.rd1.tag = URL_GLIDE
         binding.content.rd2.tag = URL_LOAD_APP
@@ -60,9 +69,54 @@ class MainActivity : AppCompatActivity() {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
             if (downloadID == id) {
-                Toast.makeText(context, context?.getString(R.string.download_complete) ?: "", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    context,
+                    context?.getString(R.string.download_complete) ?: "",
+                    Toast.LENGTH_LONG
+                ).show()
+                sendNotification()
             }
         }
+    }
+
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun sendNotification() {
+        val intent = Intent(this, DetailActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp) // replace with your app icon
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_description))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .addAction(0, getString(R.string.notification_button), pendingIntent)
+            .build()
+
+        val notificationManager = ContextCompat.getSystemService(this, NotificationManager::class.java) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
     private fun download(url: String) {
@@ -83,10 +137,11 @@ class MainActivity : AppCompatActivity() {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
-
-
+        private const val NOTIFICATION_ID = 1
         private const val URL_GLIDE = "https://github.com/bumptech/glide"
-        private const val URL_LOAD_APP = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+        private const val URL_LOAD_APP =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
         private const val URL_RETROFIT = "https://github.com/square/retrofit"
+
     }
 }
